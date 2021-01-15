51024c7374aa2ffe6f83dcc8d23d844acc509602
D3h4n <shawn2012tt@live.com>
2021-01-15 14:52:34 -0400

    Created SignUp component

---
+++ frontend/src/components/SignUp.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+import { SignUpData } from '../defintions'
+
+export default function SignUp() {
+    const [formData, setData] = useState<SignUpData>({userName: '', passWord: '', displayName: ''})
+    const textLimit = 100;
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
