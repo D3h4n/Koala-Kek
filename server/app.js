97b08bd6bf787a739922336b06f5f331fd20471f
D3h4n <shawn2012tt@live.com>
2021-01-17 02:45:32 -0400

    Restructured folder to push react frontend from express backend

--- server/app.js
+++ server/app.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 
 const apiRouter = require('./routes/apiRouter');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static("public"));
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(path.join(__dirname, '..', '/build/index.html'));
 })
 
 app.use('/api', apiRouter)
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
 app.listen(process.env.PORT || 5050);
