7531f2958a998bc4c7b03cf24deb18442319f7c8
D3h4n <shawn2012tt@live.com>
2021-01-17 02:48:43 -0400

    Commented code

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const apiRouter = require('./routes/apiRouter');
 
 const app = express();
 
+// MiddleWare and Static Files
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(express.static("public"));
 app.use(morgan('dev'));
@@ -14,10 +15,7 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '/build/index.html'));
-})
-
+// Routes
 app.use('/api', apiRouter)
 
 app.use((req, res, next) => {
