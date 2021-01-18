8758ddb38e70c857b43ffeafb205470b1a726e4c
D3h4n <shawn2012tt@live.com>
2021-01-18 00:30:11 -0400

    Updated package.json

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 
+const port = process.env.PORT || 5050;
+
 const apiRouter = require('./routes/apiRouter');
 
 const app = express();
@@ -22,4 +24,6 @@ app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
-app.listen(process.env.PORT || 5050);
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`)
+});
