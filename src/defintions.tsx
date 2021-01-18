197e74d192554656933870cac93221cf82531e79
D3h4n <shawn2012tt@live.com>
2021-01-18 01:30:22 -0400

    All modules now stored in same place

--- src/defintions.tsx
+++ src/defintions.tsx
@@ -27,7 +27,7 @@ export interface SignUpData{
     displayName: string
 }
 
-export const defaultIcon = "https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png"
-export const defaultUser = {userID: '-1', userName: '', displayName: 'User not found', icon: defaultIcon }
+export const defaultIcon = "https://www.xovi.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png";
+export const defaultUser = {userID: '-1', userName: '', displayName: 'User not found', icon: defaultIcon };
 export const apiSrc = 'http://localhost:5050/api';
 export const loginKey = 'user-id';
