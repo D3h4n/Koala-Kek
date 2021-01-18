540b0f967a301a2f63ef773802965bfa850bae20
D3h4n <shawn2012tt@live.com>
2021-01-18 00:53:05 -0400

    Added favicon, assests folder and manifest.json

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -120,6 +120,7 @@ export default function App() {
                 </Route>
             </Switch>
             </Router>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </div>
     )
 }
