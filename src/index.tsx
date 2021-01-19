2c6dc761916163e660669bbf70359bb13fb7a170
D3h4n <shawn2012tt@live.com>
2021-01-19 18:22:20 -0400

    Moved styles for credits to index.scss

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,6 @@ ReactDOM.render(
     <Router>
         <App />
     </Router>
-    <div style={{position: 'absolute', bottom:'0', right:'10px', color:'lightgray', fontSize: '12px'}}>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    <div className="credit">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 </React.StrictMode>, 
 document.getElementById('root'));
